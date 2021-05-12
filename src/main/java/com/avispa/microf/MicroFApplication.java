fab0595d1517a77588b87a39aae662dd6a37d300
Rafał Hiszpański <rharryh@gmail.com>
2021-05-12 22:27:04 +0200

    Simplified configuration by moving CMS parts to CMS configuration

--- src/main/java/com/avispa/microf/MicroFApplication.java
+++ src/main/java/com/avispa/microf/MicroFApplication.java
@@ -1,32 +1,19 @@
 package com.avispa.microf;
 
-import com.avispa.cms.model.filestore.FileStore;
-import com.avispa.cms.model.filestore.FileStoreRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.avispa.cms.CmsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-@SpringBootApplication
-@EnableAsync
-@ComponentScan("com.avispa")
-@EntityScan("com.avispa") // required to use CMS entities
-@EnableJpaRepositories("com.avispa") // required to use CMS repositories
+@SpringBootApplication(scanBasePackages = "com.avispa")
+@EntityScan("com.avispa.cms.model") // required to use CMS entities
+@EnableJpaRepositories("com.avispa.cms.model") // required to use CMS repositories
 @PropertySource("classpath:application.properties")
 @PropertySource("file:./microf.properties")
-@Slf4j
+@Import(CmsConfiguration.class)
 public class MicroFApplication {
 	/*@Bean
 	@Description("Spring Message Resolver")
@@ -35,45 +22,6 @@ public class MicroFApplication {
 		messageSource.setBasename("application");
 		return messageSource;
 	}*/
-	@Autowired
-	private FileStoreRepository fileStoreRepository;
-
-	@Bean
-	public FileStore getFileStore(@Value("${microf.fileStoreName}") String fileStoreName,
-								  @Value("${microf.defaultFileStoreName}") String defaultFileStoreName,
-								  @Value("${microf.defaultFileStorePath}") String defaultFileStorePath) {
-		FileStore fileStore = fileStoreRepository.findByName(fileStoreName);
-		if(null == fileStore) {
-			fileStore = fileStoreRepository.findByName(defaultFileStoreName);
-			if(null == fileStore) {
-				fileStore = createDefaultFileStore(defaultFileStoreName, Path.of(System.getProperty("user.home"), defaultFileStorePath).toString());
-			}
-		}
-
-		createFileStorePath(fileStore);
-
-		return fileStore;
-	}
-
-	private FileStore createDefaultFileStore(String defaultFileStoreName, String defaultFileStorePath) {
-		FileStore fileStore;
-
-		fileStore = new FileStore();
-		fileStore.setName(defaultFileStoreName);
-		fileStore.setRootPath(defaultFileStorePath);
-		fileStoreRepository.save(fileStore);
-
-		return fileStore;
-	}
-
-	private void createFileStorePath(FileStore fileStore) {
-		Path fp = Paths.get(fileStore.getRootPath());
-		try {
-			Files.createDirectories(fp);
-		} catch (IOException e) {
-			log.error("Can't create file store folders", e);
-		}
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroFApplication.class, args);
