d236bd5869d815c4756be8793fa536b039f41a92
Rafał Hiszpański <rharryh@gmail.com>
2021-10-05 20:49:58 +0200

    Setting serial number is now automated process based on the strategy selected in the configuration Added two serial number strategies implementations Added tests for checking invoice mapper

--- src/main/java/com/avispa/microf/model/invoice/InvoiceRepository.java
+++ src/main/java/com/avispa/microf/model/invoice/InvoiceRepository.java
@@ -1,6 +1,7 @@
 package com.avispa.microf.model.invoice;
 
 import com.avispa.ecm.model.EcmEntityRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface InvoiceRepository extends EcmEntityRepository<Invoice> {
+    @Query("select coalesce(max(i.serialNumber), 0) from Invoice i")
+    int findMaxSerialNumber();
+
+    @Query("select coalesce(max(i.serialNumber), 0) from Invoice i where month(i.invoiceDate) = ?1")
+    int findMaxSerialNumberByMonth(int month);
 }
