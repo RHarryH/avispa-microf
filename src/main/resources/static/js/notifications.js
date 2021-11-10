07b130fd0feb8b586b2abe80f5d6a87f9f484162
Rafał Hiszpański <rharryh@gmail.com>
2021-11-10 23:15:42 +0100

    Added support for notifications Deletion of invoice prints notification

---
+++ src/main/resources/static/js/notifications.js
@@ -0,0 +1,92 @@
+function successNotification(message) {
+    createNotification("success", message);
+}
+
+function infoNotification(message) {
+    createNotification("info", message);
+}
+
+function warningNotification(message) {
+    createNotification("warning", message);
+}
+
+function errorNotification(message) {
+    createNotification("error", message);
+}
+
+function createNotification(type, message) {
+    let container = createContainer(type);
+    createHeader(container, type);
+    createContent(container, message);
+    initToast(container);
+}
+
+function createContainer(type) {
+    let container = $("<div></div>");
+    container.addClass("toast");
+    //container.attr("data-bs-autohide", false);
+    if (type === "error") {
+        container.attr("alert", "status");
+        container.attr("aria-live", "assertive");
+    } else {
+        container.attr("role", "status");
+        container.attr("aria-live", "polite");
+    }
+    return container;
+}
+
+function createHeader(container, type) {
+    let header = $("<div></div>");
+    header.addClass("toast-header").addClass(type);
+    createHeaderTitle(header, type);
+    header.append("<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"toast\" aria-label=\"Close\"></button>");
+    container.append(header);
+}
+
+function createHeaderTitle(header, type) {
+    let title = $("<strong></strong>")
+    title.addClass("me-auto bi").addClass(getIconClass(type));
+    title.html("&nbsp;" + getHeaderTitle(type));
+    header.append(title);
+}
+
+function getIconClass(type) {
+    switch(type) {
+        case "success":
+            return "bi-check-circle-fill";
+        case "info":
+            return "bi-info-circle-fill";
+        case "warning":
+        case "error":
+            return "bi-exclamation-triangle-fill";
+    }
+}
+
+function getHeaderTitle(type) {
+    switch(type) {
+        case "success":
+            return "Success";
+        case "info":
+            return "Information";
+        case "warning":
+            return "Warning";
+        case "error":
+            return "Error";
+    }
+}
+
+function createContent(container, message) {
+    let content = $("<div></div>");
+    content.addClass("toast-body");
+    content.html(message);
+    container.append(content);
+}
+
+function initToast(container) {
+    $("#notifications").append(container);
+    let toast = bootstrap.Toast.getOrCreateInstance(container);
+    toast.show();
+    container.on("hidden.bs.toast", function () {
+        container.remove();
+    });
+}
