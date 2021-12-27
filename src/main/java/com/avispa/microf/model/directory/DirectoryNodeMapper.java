afea77df993e2e0aba684a4286b9c2bf705de267
Rafał Hiszpański <rharryh@gmail.com>
2021-12-27 15:23:09 +0100

    Upgraded mapstruct to version 1.4.2Final

--- src/main/java/com/avispa/microf/model/directory/DirectoryNodeMapper.java
+++ src/main/java/com/avispa/microf/model/directory/DirectoryNodeMapper.java
@@ -11,13 +11,13 @@ import java.util.UUID;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface DirectoryNodeMapper {
-    @Mapping(source = "folder.id", target= "parent", defaultValue = "#", qualifiedByName = "uuidToString")
+    @Mapping(source = "folder.id", target= "parent", defaultExpression = "java(\"#\")", qualifiedByName = "uuidToString")
     @Mapping(source = "id", target= "id", qualifiedByName = "uuidToString")
     @Mapping(source = "objectName", target= "text")
     @Mapping(target="type", expression = "java(null != folder.getFolder() ? \"folder\" : \"root\")")
     DirectoryNode convert(Folder folder);
 
-    @Mapping(source = "folder.id", target= "parent", defaultValue = "#", qualifiedByName = "uuidToString")
+    @Mapping(source = "folder.id", target= "parent", defaultExpression = "java(\"#\")", qualifiedByName = "uuidToString")
     @Mapping(source = "id", target= "id", qualifiedByName = "uuidToString")
     @Mapping(source = "objectName", target= "text")
     @Mapping(source = "document", target="type", qualifiedByName = "getType")
