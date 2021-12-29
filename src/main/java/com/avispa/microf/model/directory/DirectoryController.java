212667da0a81a4c315a4512b64ff44e8b7dee235
Rafał Hiszpański <rharryh@gmail.com>
2021-12-29 19:02:26 +0100

    Added export of directory structure to zip file

--- src/main/java/com/avispa/microf/model/directory/DirectoryController.java
+++ src/main/java/com/avispa/microf/model/directory/DirectoryController.java
@@ -1,5 +1,7 @@
 package com.avispa.microf.model.directory;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,12 @@ import com.avispa.ecm.model.document.Document;
 import com.avispa.ecm.model.document.DocumentService;
 import com.avispa.ecm.model.folder.Folder;
 import com.avispa.ecm.model.folder.FolderService;
+import com.avispa.microf.model.zip.ZipService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("directory")
 @RequiredArgsConstructor
+@Slf4j
 public class DirectoryController {
 
     private final FolderService folderService;
     private final DocumentService documentService;
+    private final ZipService zipService;
     private final DirectoryNodeMapper directoryNodeMapper;
 
     @GetMapping
@@ -43,5 +52,15 @@ public class DirectoryController {
         for(Folder folder : folders) {
             directoryNodes.add(directoryNodeMapper.convert(folder));
         }
+    }
+
+    @GetMapping(params = "export", produces = "application/zip")
+    public ResponseEntity<Resource> exportStructure() {
+        ByteArrayResource resource = new ByteArrayResource(zipService.getZippedStructure());
+
+        return ResponseEntity.ok()
+                .header("Content-disposition", "attachment; filename=structure-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")) + ".zip")
+                .contentLength(resource.contentLength())
+                .body(resource);
     }
 }
