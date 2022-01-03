f42012a8b3100d3fbf81368bd4ee698aa797d876
Rafał Hiszpański <rharryh@gmail.com>
2022-01-04 00:14:03 +0100

    Greatly improved and simplified BigDecimal handling in Dto

--- src/main/java/com/avispa/microf/model/invoice/position/PositionDto.java
+++ src/main/java/com/avispa/microf/model/invoice/position/PositionDto.java
@@ -1,11 +1,16 @@
 package com.avispa.microf.model.invoice.position;
 
 import com.avispa.microf.model.Dto;
+import com.avispa.microf.util.FormatUtils;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.validation.constraints.Digits;
+import java.math.BigDecimal;
 
 /**
- * Position on the invoice. Position name is object name.
+ * Position on the invoice.
  *
  * @author Rafał Hiszpański
  */
@@ -13,9 +18,20 @@ import lombok.Setter;
 @Setter
 public class PositionDto implements Dto {
     private String objectName;
-    private String quantity = "1";
+
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    @Digits(integer=5, fraction=3)
+    private BigDecimal quantity = BigDecimal.ONE;
+
     private String unit;
-    private String unitPrice;
-    private String discount = "0.00";
+
+    @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = FormatUtils.MONEY_DECIMAL_FORMAT)
+    @Digits(integer=7, fraction=2)
+    private BigDecimal unitPrice;
+
+    @NumberFormat(style = NumberFormat.Style.PERCENT)
+    @Digits(integer=7, fraction=2)
+    private BigDecimal discount = new BigDecimal("0.00");
+
     private String vatRate;
 }
