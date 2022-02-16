e072ebf56a4ef139b8caec2401d3dc318ef20b5e
Rafał Hiszpański <rharryh@gmail.com>
2022-02-16 22:41:59 +0100

    Added support for custom validation Added validation for Vat Identification Number (NIP)

---
+++ src/main/java/com/avispa/microf/util/validation/ValidationService.java
@@ -0,0 +1,32 @@
+package com.avispa.microf.util.validation;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Rafał Hiszpański
+ */
+@Service
+public class ValidationService {
+
+    private ValidationService() {
+    }
+
+    public boolean isValidVatIdentificationNumber(String nip) {
+        if (nip.length() == 13) {
+            nip = nip.replace("-", "");
+        }
+        if (nip.length() != 10) {
+            return false;
+        }
+        int[] weights = {6, 5, 7, 2, 3, 4, 5, 6, 7};
+        try {
+            int sum = 0;
+            for (int i = 0; i < weights.length; ++i) {
+                sum += Integer.parseInt(nip.substring(i, i + 1)) * weights[i];
+            }
+            return (sum % 11) == Integer.parseInt(nip.substring(9, 10));
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
