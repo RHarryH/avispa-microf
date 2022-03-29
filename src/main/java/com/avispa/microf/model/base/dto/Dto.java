4d56fc89143a983867030e1f2f230995b4a71675
Rafał Hiszpański <rharryh@gmail.com>
2022-03-29 22:35:08 +0200

    Fixed modals update Removed need of providing entities and dtos in controllers, they are resolved automatically

--- src/main/java/com/avispa/microf/model/base/dto/Dto.java
+++ src/main/java/com/avispa/microf/model/base/dto/Dto.java
@@ -1,8 +1,13 @@
 package com.avispa.microf.model.base.dto;
 
+import java.util.UUID;
+
 /**
  * @author Rafał Hiszpański
  */
 public interface Dto {
     String EMPTY_STRING_REGEX = "^$|";
+
+    UUID getId();
+    void setId(UUID id);
 }
