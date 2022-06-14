25064edd015b478806dd89bdfaf7801b2de4c173
Rafał Hiszpański <rharryh@gmail.com>
2022-06-14 23:45:29 +0200

    Added generic validation of IBAN numbers and BBAN number for Poland (NRB)

--- src/main/java/com/avispa/microf/util/validation/account/IBANConstraint.java
+++ src/main/java/com/avispa/microf/util/validation/account/IBANConstraint.java
@@ -1,4 +1,4 @@
-package com.avispa.microf.util.validation;
+package com.avispa.microf.util.validation.account;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
@@ -9,11 +9,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Constraint(validatedBy = VatIdentificationNumberValidator.class)
+@Constraint(validatedBy = IBANConstraintValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface VatIdentificationNumberConstraint {
-    String message() default "Vat Identification Number is invalid";
+public @interface IBANConstraint {
+    String message() default "IBAN is invalid";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
