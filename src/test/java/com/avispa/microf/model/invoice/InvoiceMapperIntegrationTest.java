1e0909940b7e670a63e3c60b289c967ee1836536
Rafał Hiszpański <rharryh@gmail.com>
2022-07-02 13:02:42 +0200

    Fixed tests

--- src/test/java/com/avispa/microf/model/invoice/InvoiceMapperIntegrationTest.java
+++ src/test/java/com/avispa/microf/model/invoice/InvoiceMapperIntegrationTest.java
@@ -2,9 +2,13 @@ package com.avispa.microf.model.invoice;
 
 import com.avispa.ecm.model.EcmObjectRepository;
 import com.avispa.ecm.model.configuration.dictionary.DictionaryValue;
+import com.avispa.microf.model.bankaccount.BankAccount;
+import com.avispa.microf.model.bankaccount.BankAccountRepository;
 import com.avispa.microf.model.customer.Customer;
 import com.avispa.microf.model.customer.CustomerRepository;
 import com.avispa.microf.model.invoice.position.PositionMapperImpl;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +21,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Rafał Hiszpański
@@ -36,6 +42,16 @@ class InvoiceMapperIntegrationTest {
     @MockBean
     private CustomerRepository customerRepository;
 
+    @MockBean
+    private BankAccountRepository bankAccountRepository;
+
+    @BeforeEach
+    public void init() {
+        BankAccount bankAccount = getBankAccount();
+
+        when(bankAccountRepository.getById(any(UUID.class))).thenReturn(bankAccount);
+    }
+
     @Test
     void givenInvoiceToDto_whenMaps_thenCorrect() {
         Customer customer = new Customer();
@@ -45,6 +61,7 @@ class InvoiceMapperIntegrationTest {
         invoice.setSeller(customer);
         invoice.setBuyer(customer);
         invoice.setComments("Test comment");
+        invoice.setBankAccount(getBankAccount());
 
         InvoiceDto invoiceDto = mapper.convertToDto(invoice);
 
@@ -61,6 +78,7 @@ class InvoiceMapperIntegrationTest {
         invoice.setBuyer(customer);
         invoice.setIssueDate(LocalDate.of(2011, 11, 10));
         invoice.setServiceDate(LocalDate.of(2011, 11, 10));
+        invoice.setBankAccount(getBankAccount());
 
         InvoiceDto invoiceDto = mapper.convertToDto(invoice);
 
@@ -74,6 +92,7 @@ class InvoiceMapperIntegrationTest {
         invoiceDto.setSeller(UUID.randomUUID().toString());
         invoiceDto.setBuyer(UUID.randomUUID().toString());
         invoiceDto.setComments("Test comment");
+        invoiceDto.setBankAccount(UUID.randomUUID().toString());
 
         Invoice invoice = mapper.convertToEntity(invoiceDto);
 
@@ -87,10 +106,19 @@ class InvoiceMapperIntegrationTest {
         invoiceDto.setBuyer(UUID.randomUUID().toString());
         invoiceDto.setIssueDate("2011-11-10");
         invoiceDto.setServiceDate("2011-11-10");
+        invoiceDto.setBankAccount(UUID.randomUUID().toString());
 
         Invoice invoice = mapper.convertToEntity(invoiceDto);
 
         assertEquals(invoiceDto.getIssueDate(), invoice.getIssueDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
         assertEquals(invoiceDto.getServiceDate(), invoice.getServiceDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+
+    private BankAccount getBankAccount() {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setId(UUID.randomUUID());
+        bankAccount.setAccountNumber(RandomStringUtils.randomAlphanumeric(24));
+        bankAccount.setBankName("test bank");
+        return bankAccount;
     }
 }
