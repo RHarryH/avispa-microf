d735617ed796a475e76a66055bad0a112b6ac8d3
Rafał Hiszpański <rharryh@gmail.com>
2022-07-15 09:51:25 +0200

    Extracted getting of entity service or mapper based on class to separate service

--- src/main/java/com/avispa/microf/model/ui/widget/list/ListWidgetService.java
+++ src/main/java/com/avispa/microf/model/ui/widget/list/ListWidgetService.java
@@ -5,9 +5,7 @@ import com.avispa.ecm.model.configuration.display.DisplayService;
 import com.avispa.ecm.model.document.Document;
 import com.avispa.ecm.model.type.Type;
 import com.avispa.ecm.model.type.TypeService;
-import com.avispa.microf.model.base.IBaseService;
 import com.avispa.microf.model.base.dto.CommonDto;
-import com.avispa.microf.model.base.dto.Dto;
 import com.avispa.microf.model.base.dto.DtoObject;
 import com.avispa.microf.model.base.dto.DtoService;
 import com.avispa.microf.model.ui.widget.list.config.ListWidgetConfig;
@@ -15,14 +13,12 @@ import com.avispa.microf.model.ui.widget.list.config.ListWidgetRepository;
 import com.avispa.microf.model.ui.widget.list.dto.ListDataDto;
 import com.avispa.microf.model.ui.widget.list.dto.ListWidgetDto;
 import com.avispa.microf.model.ui.widget.list.mapper.ListDataDtoMapper;
+import com.avispa.microf.util.GenericService;
 import com.avispa.microf.util.TypeNameUtils;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.reflect.TypeUtils;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.TypeVariable;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -38,7 +34,7 @@ public class ListWidgetService {
     private final ListWidgetRepository repository;
     private final ListDataDtoMapper listDataDtoMapper;
 
-    private final List<IBaseService<? extends EcmObject, ? extends Dto>> services;
+    private final GenericService genericService;
 
     public ListWidgetDto getAllDataFrom(String resourceId) {
         String typeName = TypeNameUtils.convertResourceIdToTypeName(resourceId);
@@ -88,21 +84,7 @@ public class ListWidgetService {
                 .collect(Collectors.toList());
     }
 
-    @SuppressWarnings("unchecked")
     private List<CommonDto> findAll(Class<? extends EcmObject> entityClass) {
-        IBaseService<EcmObject, Dto> foundService =
-                (IBaseService<EcmObject, Dto>) services.stream().filter(m -> {
-                    Class<?> serviceEntityClass = getServiceEntity(m.getClass());
-                    return entityClass.equals(serviceEntityClass);
-                }).findFirst().orElseThrow();
-
-        return foundService.findAll();
-    }
-
-    private Class<?> getServiceEntity(Class<?> serviceClass) {
-        Map<TypeVariable<?>, java.lang.reflect.Type> typeArgs  = TypeUtils.getTypeArguments(serviceClass, IBaseService.class);
-        TypeVariable<?> argTypeParam =  IBaseService.class.getTypeParameters()[0];
-        java.lang.reflect.Type argType = typeArgs.get(argTypeParam);
-        return TypeUtils.getRawType(argType, null);
+        return genericService.getService(entityClass).findAll();
     }
 }
