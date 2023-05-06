d9ea2a94f30ff85a8d4b3121fd3c5a77679eef8e
Rafał Hiszpański <rharryh@gmail.com>
2023-05-06 18:24:21 +0200

    Improved loading of avispa specific data

--- src/main/java/com/avispa/microf/model/base/mapper/IEntityDtoMapper.java
+++ src/main/java/com/avispa/microf/model/base/mapper/IEntityDtoMapper.java
@@ -3,13 +3,12 @@ package com.avispa.microf.model.base.mapper;
 import com.avispa.ecm.model.EcmObject;
 import com.avispa.microf.model.base.dto.Dto;
 import org.mapstruct.MappingTarget;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Rafał Hiszpański
  */
-@Transactional(propagation = Propagation.MANDATORY)
+@Transactional
 public interface IEntityDtoMapper<T extends EcmObject, D extends Dto> {
     D convertToDto(T entity);
     T convertToEntity(D dto);
