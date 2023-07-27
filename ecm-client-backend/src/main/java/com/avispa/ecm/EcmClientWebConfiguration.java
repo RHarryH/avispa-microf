d74dd6c027fec1e4388f9838f3c5688974ca93a1
Rafał Hiszpański <rharryh@gmail.com>
2023-07-27 15:45:26 +0200

    Added CORS support

---
+++ ecm-client-backend/src/main/java/com/avispa/ecm/EcmClientWebConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * Avispa μF - invoice generating software built on top of Avispa ECM
+ * Copyright (C) 2023 Rafał Hiszpański
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.avispa.ecm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Rafał Hiszpański
+ */
+@Configuration
+public class EcmClientWebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+    }
+}
