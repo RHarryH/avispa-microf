36cd5f9a678d3e08614fd931280fba86b976d5e6
Rafał Hiszpański <rharryh@gmail.com>
2023-09-23 10:45:03 +0200

    Updated microF endpoints to be RESTful

--- ecm-client-backend/src/main/java/com/avispa/ecm/EcmClientWebConfiguration.java
+++ ecm-client-backend/src/main/java/com/avispa/ecm/EcmClientWebConfiguration.java
@@ -29,6 +29,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class EcmClientWebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("GET", "HEAD", "POST", "DELETE");
     }
 }
--- microf-custom/src/main/java/com/avispa/microf/model/customer/CustomerController.java
+++ microf-custom/src/main/java/com/avispa/microf/model/customer/CustomerController.java
@@ -22,20 +22,19 @@ import com.avispa.ecm.model.base.controller.BaseController;
 import com.avispa.microf.model.customer.exception.CustomerInUseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 /**
  * @author Rafał Hiszpański
  */
+@RestController
+@RequestMapping("/v1/customer")
 @Slf4j
-@Controller
-@RequestMapping("/customer")
 public class CustomerController extends BaseController<Customer, CustomerDto, CustomerService> {
 
     public CustomerController(CustomerService service) {
@@ -43,8 +42,7 @@ public class CustomerController extends BaseController<Customer, CustomerDto, Cu
     }
 
     @Override
-    @DeleteMapping("/delete/{id}")
-    @ResponseBody
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") UUID id) {
         try {
             super.delete(id);
