fd7b7f79d5c90e282f33edc7e21e80af471590b9
Rafał Hiszpański <rharryh@gmail.com>
2023-12-01 23:05:12 +0100

    Generic error handling for all runtime exceptions

---
+++ ecm-client-backend/src/main/java/com/avispa/ecm/util/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Avispa μF - invoice generating software built on top of Avispa ECM
+ * Copyright (C) 2023 Rafał Hiszpański
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.avispa.ecm.util.error;
+
+
+import com.avispa.ecm.util.exception.EcmException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EcmException.class)
+    public void handleEcmException() {
+        // do nothing
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Unexpected issue has happened. Please contact with the administrator.")
+    public void handleRuntimeException(RuntimeException e) {
+        log.error("Generic runtime exception has been thrown", e);
+    }
+}
