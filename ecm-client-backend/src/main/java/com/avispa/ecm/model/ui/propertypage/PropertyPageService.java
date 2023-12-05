f6c38d69f2a532a6bf7fb99896e0a7040906f2a4
Rafał Hiszpański <rharryh@gmail.com>
2023-12-05 21:59:59 +0100

    Distinction between insertion and update when retrieving property pages

--- ecm-client-backend/src/main/java/com/avispa/ecm/model/ui/propertypage/PropertyPageService.java
+++ ecm-client-backend/src/main/java/com/avispa/ecm/model/ui/propertypage/PropertyPageService.java
@@ -25,6 +25,7 @@ import com.avispa.ecm.model.configuration.context.ContextService;
 import com.avispa.ecm.model.configuration.propertypage.PropertyPage;
 import com.avispa.ecm.model.configuration.propertypage.content.PropertyPageContent;
 import com.avispa.ecm.model.configuration.propertypage.content.mapper.PropertyPageMapper;
+import com.avispa.ecm.model.configuration.propertypage.content.mapper.PropertyPageMapperConfigurer;
 import com.avispa.ecm.model.configuration.upsert.Upsert;
 import com.avispa.ecm.util.exception.RepositoryCorruptionError;
 import lombok.RequiredArgsConstructor;
@@ -48,12 +49,14 @@ public class PropertyPageService {
      * Returned page is always available to edit.
      * @param entityClass ECM object which upsert configuration will be used
      * @param contextDto DTO object used as context for property page
+     * @param edit true if page will be used for editing existing object
      * @return property page content with values
      */
-    public PropertyPageContent getPropertyPage(Class<? extends EcmObject> entityClass, Dto contextDto) {
+    public PropertyPageContent getPropertyPage(Class<? extends EcmObject> entityClass, Dto contextDto, boolean edit) {
         return contextService.getConfiguration(entityClass, Upsert.class)
                 .map(Upsert::getPropertyPage)
-                .map(propertyPage -> propertyPageMapper.convertToContent(propertyPage, contextDto, false))
+                .map(propertyPage -> propertyPageMapper.convertToContent(
+                        getPropertyPageMode(edit), propertyPage, contextDto))
                 .orElseThrow(() -> new RepositoryCorruptionError(MISSING_PROPERTY_PAGE_ERROR));
     }
 
@@ -61,24 +64,40 @@ public class PropertyPageService {
      * Get property page from name only (like select source)
      * @param name property page name
      * @param context context object used to fill property page with values
+     * @param edit true if page will be used for editing existing object
      * @return
      */
-    public PropertyPageContent getPropertyPage(String name, Object context) {
+    public PropertyPageContent getPropertyPage(String name, Object context, boolean edit) {
         return propertyPageRepository.findByObjectName(name)
-                .map(propertyPage -> propertyPageMapper.convertToContent(propertyPage, context, false))
+                .map(propertyPage -> propertyPageMapper.convertToContent(getPropertyPageMode(edit), propertyPage, context))
                 .orElseThrow(() -> new RepositoryCorruptionError(MISSING_PROPERTY_PAGE_ERROR));
     }
 
     /**
-     * Get property page by matching it with provided object type. Same object is later used to extract the values.
-     * Useful for read operations when conversion to DTO is not required.
+     * Get property page mode based on simple boolean information
+     *
+     * @param edit
+     * @return
+     */
+    private static PropertyPageMapperConfigurer getPropertyPageMode(boolean edit) {
+        return edit ?
+                PropertyPageMapperConfigurer.edit() :
+                PropertyPageMapperConfigurer.insert();
+    }
+
+    /**
+     * Get readonly property page by matching it with provided object type. Same object is later used to extract the
+     * values. Useful for read operations when conversion to DTO is not required.
      * @param ecmObject object, which type will be used to find matching property page in the context
-     * @param readonly when true, all controls should not be available for editing
      * @return property page content with values
      */
-    public PropertyPageContent getPropertyPage(EcmObject ecmObject, boolean readonly) {
+    public PropertyPageContent getReadonlyPropertyPage(EcmObject ecmObject) {
         return contextService.getConfiguration(ecmObject, PropertyPage.class)
-                .map(propertyPage -> propertyPageMapper.convertToContent(propertyPage, ecmObject, readonly))
+                .map(propertyPage ->
+                        propertyPageMapper.convertToContent(
+                                PropertyPageMapperConfigurer.readonly(),
+                                propertyPage,
+                                ecmObject))
                 .orElseThrow(() -> new RepositoryCorruptionError(MISSING_PROPERTY_PAGE_ERROR));
     }
 }
